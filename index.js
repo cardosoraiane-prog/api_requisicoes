d53a2dee07275d7d7344f04581ec042b9f072ed4
SeuNome <SeuEmail>
2026-05-18 12:44:11 -0300

    modified:   index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
+const cors = require("cors");
+    app.use(cors());
 const port = 3000;
 
 app.use(express.json());
